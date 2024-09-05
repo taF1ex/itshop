dbccf80e7639c09283d05925601147b40c2ba4f7
t <wongsaphan.a@kkumail.com>
2024-09-05 17:19:43 +0700

    change function b

---
+++ app/Models/ProductType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductType extends Model
+{
+    use HasFactory;
+protected $table = 'products';
+protected $primaryKey = 'id';
+protected $guarded = [];
+public function productType()
+{
+return $this->belongsTo(ProductType::class, 'product_type_id');
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WebController;
-
+use App\Http\Controllers\ProductTypeController;
+use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -19,5 +20,8 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::get('/myshop', [WebController::class, 'myshop'])->name("web.myshop");
+
+Route::resource('product_types', ProductTypeController::class);
+Route::resource('product', ProductController::class);
 
 require __DIR__.'/auth.php';
